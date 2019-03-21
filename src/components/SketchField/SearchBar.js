adbba1d5c9ec5c88a051fa114922da1968932157
Waldyn Benbenek <wben1201@gmail.com>
2019-03-21 13:57:34 -0500

    it works kinda.

--- src/components/SketchField/SearchBar.js
+++ src/components/SketchField/SearchBar.js
@@ -21,20 +21,36 @@ const styles = theme => ({
 class SimplePopover extends React.Component {
     state = {
         search: {
-          title: '',
-          part: '',  
+            title: '',
+            part: '',
         },
         anchorEl: null,
-    };
+        popDisplay: [],
+        location: null,
+        shut: undefined,
+        run: false,
 
-    handleClick = event => {
-        event.preventDefault()
+    };
+    componentDidMount() {
+
+
+        this.props.dispatch({ type: 'SEND_SEARCH', payload: { title: this.state.search.title, part: 'costume' } })
+    }
+    handleClick = (event) => {
+
+
         this.setState({
             ...this.state,
-            anchorEl: event.currentTarget,
-        });
-        this.props.dispatch({type:'SEND_SEARCH', payload: this.state.search})
-    };
+            anchorEl: event.currentTarget
+        })
+
+
+    }
+
+
+
+
+
 
     handleClose = () => {
         this.setState({
@@ -42,54 +58,47 @@ class SimplePopover extends React.Component {
             anchorEl: null,
         });
     };
-handleChangeFor=(key)=>(event)=>{
+    handleChangeFor = (key) => (event) => {
 
-    this.setState({
-        ...this.state,
-        search:{
-            ...this.state.search,
-            [key]: event.target.value
-        }
-    })
-}
+        this.setState({
+            ...this.state,
+            search: {
+                ...this.state.search,
+                [key]: event.target.value
+            }
+        })
+        this.props.dispatch({ type: 'SEND_SEARCH', payload: { title: event.target.value, part: 'costume' } })
+    }
     render() {
-       console.log(this.state.search)
+        console.log(this.state.shut, this.state.anchorEl)
         const { anchorEl } = this.state;
         const open = Boolean(anchorEl);
 
+if(this.state.query){
         return (
             <div className='search'>
-                <form onSubmit={this.handleClick}>
-            <input onChange={this.handleChangeFor('title')}></input>
-                    
-                    <div onChange={this.handleChangeFor('part')}>
-                        <div>
-                            <label>
-                                <input
-                                    type="radio"
-                                    value='costume'
-                                    name="costume"
-                                />
-                               costume
-                </label>
-                        </div>
-                        
-                    </div>
-                   
-                
+
+                {JSON.stringify(this.props.reduxStore.searchReducer)}
+                <input onChange={this.handleChangeFor('title')} />
+
                 <Button
-                type='submit'
-                value='submit'
-                    aria-owns={open ? 'simple-popper' : undefined}
+                    onClick={this.handleClick}
+                    type='submit'
+                    value='submit'
+                    ref='buttonElement'
+
+                    aria-owns={open ? this.state.shut : undefined}
                     aria-haspopup="true"
                     variant="contained"
-                    // onClick={this.handleClick}
+
                 >
                     Search
-        </Button>
-                </form>
-                <Popover style={{width: '70%'}}
+                    </Button>
+
+
+                <Popover style={{ width: '70%' }}
                     id="simple-popper"
+
                     open={open}
                     anchorEl={anchorEl}
                     onClose={this.handleClose}
@@ -103,66 +112,118 @@ handleChangeFor=(key)=>(event)=>{
                     }}
                 >
                     <List >
-                        <ListItem >
-                        
-                            <Card style={{ width: '70%' }} className="item-card">
-                                <CardActionArea>
-                                    <CardMedia
-                                        key='https://static.giantbomb.com/uploads/original/10/101494/2165401-newgb2.jpg'
-                                        component="img"
-                                        alt='batman'
-                                        className="item-img"
-                                        height="240"
-                                        image='https://static.giantbomb.com/uploads/original/10/101494/2165401-newgb2.jpg'
-                                        title='batman'
-                                        onDragStart={(event) => this.props.onDragStart(event, 'https://static.giantbomb.com/uploads/original/10/101494/2165401-newgb2.jpg' )}
-                                        draggable
-                                        className="draggable"
-                                    />
+                        <ListItem>
+                            {this.props.reduxStore.searchReducer.map(heroImage => (
+                                <Card style={{ width: '70%' }} className="item-card">
+                                    <CardActionArea>
+                                        <CardMedia
+                                            key='1'
+                                            component="img"
+                                            alt='batman'
+                                            className="item-img"
+                                            height="240"
+                                            image={heroImage.image}
+                                            title='batman'
+                                            onDragStart={(event) => this.props.onDragStart(event, heroImage.image)}
+                                            draggable
+                                            className="draggable"
+                                        />
                                     </CardActionArea>
-                                    </Card>
-                        </ListItem>
-                        <ListItem>
-                            <Card style={{ width: '70%' }} className="item-card">
-                                <CardActionArea>
-                                    <CardMedia
-                                        key='https://static.giantbomb.com/uploads/scale_medium/10/101494/2063627-bbbb.jpg'
-                                        component="img"
-                                        alt='batman'
-                                        className="item-img"
-                                        height="240"
-                                        image='https://static.giantbomb.com/uploads/scale_medium/10/101494/2063627-bbbb.jpg'
-                                        title='batman'
-                                        onDragStart={(event) => this.props.onDragStart(event, 'https://static.giantbomb.com/uploads/scale_medium/10/101494/2063627-bbbb.jpg')}
-                                        draggable
-                                        className="draggable"
-                                    />
-                                </CardActionArea>
-                            </Card>
-                        </ListItem>
-                        <ListItem>
-                            <Card style={{ width: '70%' }} className="item-card">
-                                <CardActionArea>
-                                    <CardMedia
-                                        key='https://static.giantbomb.com/uploads/scale_medium/10/101494/2005465-hhhh.jpg'
-                                        component="img"
-                                        alt='batman'
-                                        className="item-img"
-                                        height="240"
-                                        image='https://static.giantbomb.com/uploads/scale_medium/10/101494/2005465-hhhh.jpg'
-                                        title='batman'
-                                        onDragStart={(event) => this.props.onDragStart(event, 'https://static.giantbomb.com/uploads/scale_medium/10/101494/2005465-hhhh.jpg')}
-                                        draggable
-                                        className="draggable"
-                                    />
-                                </CardActionArea>
-                            </Card>
+                                </Card>
+                            ))}
                         </ListItem>
                     </List>
                 </Popover>
+
             </div>
         );
     }
+else {
+    return (
+        
+            <form>
+                <div>
+            <label>
+                <input
+                    type="radio"
+                    value='1'
+                    name="React"
+                />
+                React
+                
+                    
+                
+                </label>
+        </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value='2'
+                        name="jQuery"
+                    />
+                    jQuery
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value='3'
+                        name="Node"
+                    />
+                    Node
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value='4'
+                        name="SQL"
+                    />
+                    SQL
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value='5'
+                        name="Redux"
+                    />
+                    Redux
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value='6'
+                        name="HTML"
+                    />
+                    HTML
+                </label>
+            </div>
+      
+
+
+
+
+
+
+
+
+
+
+        <input type="submit" value="Next" />
+
+                </form >
+                
+    )
+}
+}
+
 }
 
 SimplePopover.propTypes = {
